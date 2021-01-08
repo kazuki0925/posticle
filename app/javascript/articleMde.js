f2d4bca5127053999edb95b706aeda220b6f3b9a
kazuki0925 <kazukinyo0925@gmail.com>
2021-01-08 15:15:37 +0900

    easymdeの導入

--- app/javascript/articleMde.js
+++ app/javascript/articleMde.js
@@ -1,16 +1,16 @@
 import "inline-attachment/src/inline-attachment";
 import "inline-attachment/src/codemirror-4.inline-attachment";
-import 'simplemde/dist/simplemde.min.css'
-import SimpleMDE from "simplemde";
+import EasyMDE from "easymde"
+import 'easymde/dist/easymde.min.css';
 import Rails from '@rails/ujs'
 
 window.onload = function () {
   // textareaをMarkdownエディタにする
-  const simplemde = new SimpleMDE({
+  const easymde = new EasyMDE({
     element: document.getElementById("editor"),
   });
   // エディタに画像がドラッグ&ドロップされた際の処理
-  inlineAttachment.editors.codemirror4.attach(simplemde.codemirror, {
+  inlineAttachment.editors.codemirror4.attach(easymde.codemirror, {
     uploadUrl: "/articles/attach", // POSTする宛先Url
     uploadFieldName: "image", // ファイルのフィールド名(paramsで取り出す時のkey)
     allowedTypes: ['image/jpeg', 'image/png', 'image/jpg', 'image/gif'],
